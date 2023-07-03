5af9b2d4db88c6d21abc6d450032031a090305dc
mihkskhk <Ankwk@github.com>
2023-07-03 10:34:06 +0900

    中脇終了

--- app/src/main/java/semonster2/App.java
+++ app/src/main/java/semonster2/App.java
@@ -5,10 +5,25 @@ package semonster2;
 
 public class App {
     public String getGreeting() {
-        return "おはよう世界";
+        return "こんにちは SEMonster";
     }
 
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
+    }
+}
+
+public class Monster{
+    String monster_name;
+    int rare;
+
+    public printMonster(int monster_number){
+        String monsters[] = {"デュラハン","ドラゴン","スライム"};
+        return monsters[monster_number];
+    }
+
+    public printRare(int rare_number){
+        int rare[] = {4,5,1};
+        return rare[rare_number];
     }
 }
