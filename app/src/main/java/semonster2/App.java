9ef7385cb4d87cf9dd11deef1f9f9f4da62ce1de
Kitawaki Ryuto <e1c20036@oit.ac.jp>
2023-07-03 10:10:50 +0900

    GitHub Actionsの設定

--- app/src/main/java/semonster2/App.java
+++ app/src/main/java/semonster2/App.java
@@ -5,7 +5,7 @@ package semonster2;
 
 public class App {
     public String getGreeting() {
-        return "Hello World!";
+        return "おはよう世界";
     }
 
     public static void main(String[] args) {
